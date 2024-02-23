04edd9526c73f89aaccccc0bd50b493f2789f133
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2024-02-23 18:24:47 +0100

    Changed entire website with new version

--- assets/js/animation.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    $(".fadeIn").each(function (i) {
-      $(this)
-        .delay(i * 1000)
-        .fadeIn(3000);
-    });
-  });
-});
